c3e794b1c6b45a561811f2e15daf4662d37a6f98
Deepanshu-Raghuwanshi <dipanshuraghuwanshi@gmai.com>
2023-10-05 01:11:30 +0530

    Database connection establish sucessfully on backend  and working on  home page for frontend

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+// import '@fontsource/roboto/300.css';
+import Home from './components/Home/Home'
+
+function App() {
+ 
+  return (
+    <>
+      <div id='root'>
+    <Home/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
